ebc580ba7ec40deb3b2e71195100501c16502a1f
Killian Schneider <80042061+yoschigamer@users.noreply.github.com>
2022-09-01 16:39:39 +0200

    Update ft_is_negative.c

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -1,12 +1,24 @@
-#include <stdio.h> //ligne d'inclusion de la bibliothèque stdio.h, directive de preprocesseur
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kschneid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 16:07:19 by kschneid          #+#    #+#             */
+/*   Updated: 2022/09/01 16:38:51 by kschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
 
-void ft_is_negative(int n) 
+void	ft_is_negative(int n)
 {
-    (n < 0) ? write(1, "N",1) : write(1, "P",1) ;  // si n est inférieur à 0, alors on affiche N, sinon on affiche P
-}
-
-int main()
-{
-    ft_is_negative(-10); // affiche N
-    return 0; // 0 est la valeur de retour de la fonction main
+	if (n < 0)
+	{
+		write(1, "N", 1);
+	}
+	else
+	{
+		write(1, "P", 1);
+	}
 }
