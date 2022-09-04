d03dcafe7ac1ee57976e4253e52c878e5dd2f3ed
kymaloo <91215909+kymaloo@users.noreply.github.com>
2022-09-04 19:24:59 +0200

    Okay

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_negative.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kschneid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tgaspard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/01 16:07:19 by kschneid          #+#    #+#             */
-/*   Updated: 2022/09/01 16:38:51 by kschneid         ###   ########.fr       */
+/*   Created: 2022/09/01 15:07:27 by tgaspard          #+#    #+#             */
+/*   Updated: 2022/09/04 12:18:50 by tgaspard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_is_negative(int n)
 {
-	if (n < 0)
+	if (n >= 0)
 	{
-		write(1, "N", 1);
+		write(1, "P", 1);
 	}
 	else
 	{
-		write(1, "P", 1);
+		write(1, "N", 1);
 	}
 }
