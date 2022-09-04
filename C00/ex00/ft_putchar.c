471ae68042acb486111b6897e12c6ee166cee598
kymaloo <91215909+kymaloo@users.noreply.github.com>
2022-09-04 19:24:32 +0200

    Okay

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kschneid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tgaspard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/01 01:43:46 by kschneid          #+#    #+#             */
-/*   Updated: 2022/09/01 02:35:20 by kschneid         ###   ########.fr       */
+/*   Created: 2022/08/31 17:38:07 by tgaspard          #+#    #+#             */
+/*   Updated: 2022/09/01 14:07:56 by tgaspard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
-void	ft_putchar(char c);
-
 void	ft_putchar(char c)
-{	
+{
 	write(1, &c, 1);
 }
