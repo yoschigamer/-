c74098c0c27a27ca43537ebf108f89a8784f476a
kymaloo <91215909+kymaloo@users.noreply.github.com>
2022-09-04 19:18:58 +0200

    FIX 7 a modifier

--- Shell01/ex01/print_groups.sh
+++ Shell01/ex01/print_groups.sh
@@ -1,1 +1,1 @@
-id -Gn $FT_USER | sed "s/ /,/" | tr -d '\n'
+groups $FT_USER | tr ' ' ',' |tr -d '\n'
--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat  /etc/passwd |sed 's/:.*//' | sed '/^#/d' | rev | sort -r | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]'  | tr '\n' ',' | sed 's/,/, /g' | sed 's/.\{2\}$/./'
+cat  /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d : -f 1 | rev | sort -r | awk "NR>=$FT_LINE1 && NR<$FT_LINE2"  | tr '\n' ''' | sed '$ s/ $/./' | sed 's/ /, /g' | tr -d '\n'
